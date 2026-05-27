84feac4820667ce9d7d9548f9bdc358f280ab6c4
Luthando Mbuyane <mbylut003@myuct.ac.za>
2026-05-27 16:00:50 +0200

    Create budget_service.py

---
+++ services/budget_service.py
@@ -0,0 +1,35 @@
+from config import (
+    CAPE_TOWN_RECOMMENDED_CAP,
+    CAPE_TOWN_UPPER_CAP,
+    CAPE_TOWN_EXTREME_CAP,
+)
+
+
+def calculate_budget_guidance(
+    income: float,
+):
+
+    recommended = round(
+        income *
+        CAPE_TOWN_RECOMMENDED_CAP
+    )
+
+    stretch = round(
+        income *
+        CAPE_TOWN_UPPER_CAP
+    )
+
+    high_risk = round(
+        income *
+        CAPE_TOWN_EXTREME_CAP
+    )
+
+    return {
+
+        "recommended": recommended,
+
+        "stretch": stretch,
+
+        "high_risk": high_risk,
+
+    }
